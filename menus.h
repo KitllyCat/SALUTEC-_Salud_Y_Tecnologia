8faf908cdae0942e4807e145bcb0f8907f51313b
KitllyCat <cjchambem@unjbg.edu.pe>
2025-07-05 20:22:22 -0500

    Se añadio la cabezera de menus.cpp, para su implementacion en main.cpp

---
+++ menus.h
@@ -0,0 +1,11 @@
+#ifndef MENUS_GUI
+#define MENUS_GUI
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+void menuPrincipal();
+void menuPacientes();
+void menuHistorial();
+
+#endif 
