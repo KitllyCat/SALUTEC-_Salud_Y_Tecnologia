b78f69f506cc6e1ec721744aa83943e72565bf24
Gabriel Stefano Peñaloza Gutierrez <gpenalozag@unjbg.edu.pe>
2025-07-06 00:28:21 -0500

    search.cpp - Agregando search.cpp implementando funciones vacías.

---
+++ search.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include "search.h"
+using namespace std;
+
+void listPatients(){
+    
+}
+
+void searchPatientByName(){
+
+}
+
+void searchPatientByDNI(){
+
+}
+
+void showPatient(){
+
+}
