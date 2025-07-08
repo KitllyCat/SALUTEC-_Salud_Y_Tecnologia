4a596f1fa3144cdd0b10826589b0c6d39bc9f1c7
Gabriel Stefano Peñaloza Gutierrez <gpenalozag@unjbg.edu.pe>
2025-07-08 14:22:45 -0500

    feat: Agregar librería paciente.h y declarar variables de la función listPatients.

--- search.h
+++ search.h
@@ -1,7 +1,8 @@
 #ifndef SEARCH_H
 #define SEARCH_H
+#include "paciente.h"
 
-void listPatients();
+void listPatients(Paciente paciente[], int count); //Listado general de pacientes
 void searchPatientByName();
 void searchPatientByDNI();
 void showPatient();
