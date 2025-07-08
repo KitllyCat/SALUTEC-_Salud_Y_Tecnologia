99b79df49e8570f4e83278df92148c1fdbc3f11f
Gabriel Stefano Peñaloza Gutierrez <gpenalozag@unjbg.edu.pe>
2025-07-08 14:28:20 -0500

    feat: Agregar lógica de la función listPatients.

--- search.cpp
+++ search.cpp
@@ -3,8 +3,23 @@
 #include "search.h"
 using namespace std;
 
-void listPatients(){
-    
+void listPatients(Paciente pacient[], int count){
+    if (count != 0){
+        cout << "Listado general de pacientes" << endl;
+        for (int i = 0; i < count ; i++){
+            cout << "Paciente N°" << i + 1;
+            cout << "Nombre: " << pacient->nombre;
+            cout << "DNI: " << pacient->DNI;
+            cout << "Edad: " << pacient->edad;
+            cout << "Sexo: " << pacient->sexo;
+            cout << "Tipo de sangre: " << pacient->T_sangre;
+            cout << "Número celular: " << pacient->numero;
+            cout << "Dirección" << pacient->direccion;
+        }
+    }else {
+        cout << "ERROR - Ni un paciente registrado." << endl;
+    }
+    cout << endl;
 }
 
 void searchPatientByName(){
