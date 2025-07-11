f719bff716d0314a7f1d7a392856c3174850600c
Gabriel Stefano Peñaloza Gutierrez <gpenalozag@unjbg.edu.pe>
2025-07-11 18:25:39 -0500

    feat: Agregar función showPatient para mostrar los datos del paciente.

--- search.cpp
+++ search.cpp
@@ -3,6 +3,19 @@
 #include "search.h"
 using namespace std;
 
+void showPatient(Paciente pacient[], int count){
+    for (int i = 0; i < count ; i++){
+        cout << "Paciente N°" << i + 1;
+        cout << "Nombre: " << pacient->nombre;
+        cout << "DNI: " << pacient->DNI;
+        cout << "Edad: " << pacient->edad;
+        cout << "Sexo: " << pacient->sexo;
+        cout << "Tipo de sangre: " << pacient->T_sangre;
+        cout << "Número celular: " << pacient->numero;
+        cout << "Dirección" << pacient->direccion;
+    }
+}
+
 void listPatients(Paciente pacient[], int count){
     if (count != 0){
         cout << "Listado general de pacientes" << endl;
@@ -27,9 +40,5 @@ void searchPatientByName(){
 }
 
 void searchPatientByDNI(){
-
-}
-
-void showPatient(){
 
 }
