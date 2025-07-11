9d45029fe2ce5dbf401009a810148aea7a35caa6
Gabriel Stefano Peñaloza Gutierrez <gpenalozag@unjbg.edu.pe>
2025-07-11 18:19:38 -0500

    feat: Agregar parámetros a la función showPatients.

--- search.h
+++ search.h
@@ -2,9 +2,9 @@
 #define SEARCH_H
 #include "paciente.h"
 
-void listPatients(Paciente paciente[], int count); //Listado general de pacientes
-void searchPatientByName();
+void showPatient(Paciente pacient[], int count); //Muestra los datos de los pacientes
+void listPatients(Paciente pacient[], int count); //Listado general de pacientes
+void searchPatientByName(); //Listado por el nombre del paciente
 void searchPatientByDNI();
-void showPatient();
 
 #endif
