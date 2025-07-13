3e67ac1f5fe0b40e0f815aa8d229825d84e1ed69
maricielo <cielotapia198@gmail.com>
2025-07-13 07:59:28 -0500

    Añadir funcion buscarPorDNI a historial.h

--- historial.h
+++ historial.h
@@ -15,5 +15,6 @@ void registrarDiagnostico(Diagnostico historial[], int &total);
 void mostrarHistorial(Diagnostico historial[], int &total);
 void modificarDiagnostico(Diagnostico historial[], int total);
 void buscarPorFecha(Diagnostico historial[], int total);
+void buscarPorDNI(Diagnostico historial[], int total);
 
 #endif
