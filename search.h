09c4a184334bca64c33dcacaa7215c11a97eb27f
KitllyCat <cjchambem@unjbg.edu.pe>
2025-07-13 12:46:20 -0500

    Se actualizo el search.h con extern

--- search.h
+++ search.h
@@ -5,6 +5,9 @@
 #include <windows.h>
 #include <string>
 
+extern Paciente Registro[];
+extern int cont;
+
 void showPatient(Paciente paciente); //Muestra los datos de los pacientes
 void listPatients(Paciente paciente[], int cont); //Listado general de pacientes
 void searchPatientByName(Paciente paciente[], int cont); //Listado de pacientes ordenados por nombre
