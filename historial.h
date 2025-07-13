fc07dd446155527f100b770dc62916ef731905b3
maricielo <cielotapia198@gmail.com>
2025-07-12 19:41:59 -0500

    Añadir funcion buscarPorFecha a historial.h

--- historial.h
+++ historial.h
@@ -13,5 +13,6 @@ struct Diagnostico {
 void registrarDiagnostico(Diagnostico historial[], int &total);
 void mostrarHistorial(Diagnostico historial[], int &total);
 void modificarDiagnostico(Diagnostico historial[], int total);
+void buscarPorFecha(Diagnostico historial[], int total);
 
 #endif
